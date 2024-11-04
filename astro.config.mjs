5e25ef9578ffdabd55b041587b6e15c3cf3321d7
UlisesJaramillo <59126849+UlisesJaramillo@users.noreply.github.com>
2024-11-03 21:17:06 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.milaviksolutions.com/",
-  base: "milavik-landing-page",
   integrations: [tailwind()],
 });
